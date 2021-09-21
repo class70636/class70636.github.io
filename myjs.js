a3e060ed5f07f29c25ef285e2bed42b0b97acd55
class70636 <class70638@gmail.com>
2021-09-22 05:42:51 +0800

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -4,16 +4,30 @@ $(document).ready(function () {
         var table = $('.table');
         var tr = $('tr');
 
+        if(input.length == 0)
+            table.css("display","none");
+        else table.css("display","");
+
+        var count = 0;
+
         for (i = 0; i < tr.length; i++) {
             var td = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 var txtValue = td.textContent || td.innerText;
-                if (txtValue.indexOf(input) > -1)
+                if (txtValue.indexOf(input) > -1){
                     tr[i].style.display = "";
+                    count++;
+                }
                 else
                     tr[i].style.display = "none";
             }
         }
+
+        if(count == 0){
+            table.css("display","none");
+            $('.none-search').css("display","display");
+        }
+        else $('.none-search').css("display","none");
     });
 });
 
