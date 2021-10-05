e685b7704bf7704c7918fd0e587ca767b5cfc58f
class70636 <class70638@gmail.com>
2021-10-05 22:39:38 +0800

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -7,50 +7,57 @@ $(document).ready(function () {
             var response = JSON.parse(rp);
 
             var ques = response[0];
-            var guwan = response[1];
-            var buqhai = response[2];
+            var koi = response[1];
+            var tp = response[2]
+            var guwan = response[3];
+            var buqhai = response[4];
             //讀取答題
             let text = '<tr><th class="tg co1">題目</th><th class="tg co2">答案</th></tr>';
-            for (var i = 1; i < ques.length; i++) {
-                text += '<tr><td class="tg-1">' + ques[i][0] + '</td><td class="tg-2">' + ques[i][1] + "</td></tr>";
-            }
+            for (var i = 1; i < ques.length; i++)
+                text += '<tr class="qq"><td class="tg-1">' + ques[i][0] + '</td><td class="tg-2">' + ques[i][1] + "</td></tr>";
+            for (var i = 1; i < koi.length; i++)
+                text += '<tr class="ko"><td class="tg-1">' + koi[i][0] + '</td><td class="tg-2">' + koi[i][1] + "</td></tr>";
+            for (var i = 1; i < tp.length; i++)
+                text += '<tr class="tp"><td class="tg-1">' + tp[i][0] + '</td><td class="tg-2">' + tp[i][1] + "</td></tr>";
             document.getElementsByClassName('ques')[0].innerHTML = text;
 
             //讀取古玩
             let text2 = "";
             for (var i = 1; i < guwan.length; i++) {
-                text2 += "<tr><td>" + guwan[i][0] + "</td><td>" + guwan[i][1] + "</td></tr>";
+                text2 += '<tr class="gu"><td>' + guwan[i][0] + "</td><td>" + guwan[i][1] + "</td></tr>";
             }
             document.getElementsByClassName('guwan-body')[0].innerHTML = text2;
 
             //讀取捕快
             let text3 = ""
             for (var i = 1; i < buqhai.length; i++) {
-                text3 += "<tr><td>" + buqhai[i][0] + "</td><td>" + buqhai[i][1] + "</td></tr>";
+                text3 += '<tr class="bu"><td>' + buqhai[i][0] + "</td><td>" + buqhai[i][1] + "</td></tr>";
             }
             document.getElementsByClassName('buqhai-body')[0].innerHTML = text3;
 
-            $('.loading').css("display","none");
+            $('.loading').css("display", "none");
         }
     });
 
     var input;
 
     var tempTrs = [];
-    var counts = [0, 0, 0];
+    var counts = [0, 0, 0, 0, 0];
 
     var stb = $('.searchType');
     var type = parseInt(stb.data("search"));
 
     function searchAll() {
-        searchQues();
-        searchGuwan();
-        searchBuqhai();
+        counts[0] = search('.qq');
+        counts[1] = search('.tp');
+        counts[2] = search('.ko');
+        counts[3] = search('.gu');
+        counts[4] = search('.bu');
     }
 
-    function searchQues() {
+    function search(str) {
         var count = 0;
-        var tr = $('.ques>tbody>tr');
+        var tr = $(str);
 
         for (i = 0; i < tr.length; i++) {
             //只搜尋標題
@@ -67,61 +74,15 @@ $(document).ready(function () {
             }
         }
 
-        counts[0] = count;
+        return count;
     }
-
-    function searchGuwan() {
-        var count = 0;
-        var tr = $('.guwan>tbody>tr');
-
-        for (i = 0; i < tr.length; i++) {
-            //只搜尋標題
-            var td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                var txtValue = td.textContent || td.innerText;
-                txtValue = txtValue.replace("“", "").replace("”", "").replace("「", "").replace("」", "").replace("《", "").replace("》", "");
-                if (txtValue.indexOf(input) > -1) {
-                    tr[i].style.display = "";
-                    count++;
-                }
-                else
-                    tr[i].style.display = "none";
-            }
-        }
-
-        counts[1] = count;
-    }
-
-    function searchBuqhai() {
-        var count = 0;
-        var tr = $('.buqhai>tbody>tr');
-
-        for (i = 0; i < tr.length; i++) {
-            //只搜尋標題
-            var td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                var txtValue = td.textContent || td.innerText;
-                txtValue = txtValue.replace("“", "").replace("”", "").replace("「", "").replace("」", "").replace("《", "").replace("》", "");
-                if (txtValue.indexOf(input) > -1) {
-                    tr[i].style.display = "";
-                    count++;
-                }
-                else
-                    tr[i].style.display = "none";
-            }
-        }
-
-        counts[2] = count;
-    }
-
-
 
     $('#filterName').keyup(function () {
         sear();
     });
 
     function sear() {
-        counts = [0, 0, 0];
+        counts = [0, 0, 0, 0, 0];
         input = $('.light-table-filter').val().replace("“", "").replace("”", "").replace("「", "").replace("」", "").replace("《", "").replace("》", "");
         var table = $('.table');
         var tr = $('tr');
@@ -132,26 +93,38 @@ $(document).ready(function () {
 
         switch (type) {
             case 1:
-                searchQues();
+                counts[0] = search('.qq');
                 break;
             case 2:
-                searchGuwan();
+                counts[1] = search('.tp');
                 break;
             case 3:
-                searchBuqhai();
+                counts[2] = search('.ko');
+                break;
+            case 4:
+                counts[3] = search('.gu');
+                break;
+            case 5:
+                counts[4] = search('.bu');
                 break;
             default:
                 searchAll();
-        }
 
+        }
         if (counts[0] == 0)
-            $('.ques').css('display', 'none');
+            $('.qq').css('display', 'none');
         if (counts[1] == 0)
-            $('.guwan').css('display', 'none');
+            $('.tp').css('display', 'none');
         if (counts[2] == 0)
+            $('.ko').css('display', 'none');
+        if (counts[0] + counts[1] + counts[2] == 0)
+            $('.ques').css('display', 'none');
+        if (counts[3] == 0)
+            $('.guwan').css('display', 'none');
+        if (counts[4] == 0)
             $('.buqhai').css('display', 'none');
 
-        if (counts[0] + counts[1] + counts[2] == 0) {
+        if (counts[0] + counts[1] + counts[2] + counts[3] + counts[4] == 0) {
             table.css("display", "none");
             $('.none-search').css("display", "");
         }
@@ -159,8 +132,8 @@ $(document).ready(function () {
     }
 
     //0:全部 1:古玩 2:捕快
-    var types = ['全部', '答題', '古玩', '捕快斷案'];
-    var typeAttr = ['輸入關鍵字', '輸入題目關鍵字', '輸入古玩關鍵字', '輸入捕快案名關鍵字'];
+    var types = ['全部', '答題（雅士、飲聚）', '答題（茶話會）', '答題（錦鯉）', '古玩', '捕快斷案'];
+    var typeAttr = ['輸入關鍵字', '輸入題目關鍵字', '輸入題目關鍵字', '輸入題目關鍵字', '輸入古玩關鍵字', '輸入捕快案名關鍵字'];
 
     $('.searchType').click(function () {
         type = parseInt($(this).data("search"));
@@ -176,10 +149,6 @@ $(document).ready(function () {
     $('.rpBtn').click(function () {
         // $('#reportBg').css("display", "block");
         window.open('report.html');
-    });
-
-    $(window).scroll(function () {
-        $('#reportBg').css("marginTop", ($(window).scrollTop() + 30) + "px");
     });
 });
 
